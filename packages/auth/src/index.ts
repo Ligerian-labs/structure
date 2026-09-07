d320f6eb98c39058f8155819f23b6cceb73f9408
Brother MartianGreed <valentin@pupucecorp.com>
2026-09-07 09:41:31 +0200

    fix(auth): support browser authentication flows (#65)

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -133,6 +133,7 @@ export {
   type AuthPrimitives,
   type AuthService,
   type AuthServiceError,
+  type BeginOAuthOptions,
   type EmailSender,
   type IdentityProvisionRequest,
   type MakeAuthOptions,
